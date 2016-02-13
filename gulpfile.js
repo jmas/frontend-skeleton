6bfea9fd261375ef5007a009f23804e00f520cba
jmas <jmas.ukraine@gmail.com>
2016-02-13 11:38:03 +0200

    Update js build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp        = require('gulp'),
     gutil       = require('gulp-util'),
     sass        = require('gulp-sass'),
-    csso        = require('gulp-csso'),
-    uglify      = require('gulp-uglify'),
+    //csso        = require('gulp-csso'),
+    //uglify      = require('gulp-uglify'),
     jade        = require('gulp-jade'),
     concat      = require('gulp-concat'),
     livereload  = require('gulp-livereload'), // Livereload plugin needed: https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
@@ -14,6 +14,14 @@ var gulp        = require('gulp'),
     server      = tinylr(),
     sourcemaps  = require('gulp-sourcemaps');
 
+var browserify = require('gulp-browserify');
+var babelify = require('babelify');
+var babelPresetEs2015 = require('babel-preset-es2015');
+
+var NODE_MODULES_PATH = './node_modules';
+var FOUNDATION_PATH = NODE_MODULES_PATH + '/foundation-sites';
+var FOUNDATION_JS_PATH = FOUNDATION_PATH + '/js';
+
 gulp.task('css', function() {
     return gulp.src('src/assets/stylesheets/*.scss')
         .pipe(sourcemaps.init())
@@ -21,7 +29,6 @@ gulp.task('css', function() {
             includePaths: ['src/assets/stylesheets', 'node_modules/foundation-sites/scss'],
             errLogToConsole: true
         }))
-        //.pipe(csso())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist/assets/stylesheets/'))
         .pipe(livereload(server));
@@ -30,8 +37,19 @@ gulp.task('css', function() {
 gulp.task('js', function() {
     return gulp.src('src/assets/scripts/*.js')
         .pipe(sourcemaps.init())
-        //.pipe(uglify())
-        .pipe(concat('all.min.js'))
+        .pipe(browserify({
+            debug: true,
+            paths: [ NODE_MODULES_PATH, FOUNDATION_JS_PATH ],
+            insertGlobals: true,
+            ignore: ['jquery'],
+            transform: [
+                babelify.configure({
+                    sourceMaps: true,
+                    presets: [babelPresetEs2015]
+                })
+            ],
+            extensions: ['.js']
+        }))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist/assets/scripts/'))
         .pipe(livereload(server));
@@ -58,7 +76,7 @@ gulp.task('watch', function () {
             return console.log(err);
         }
         gulp.watch(['src/assets/stylesheets/*.scss', 'src/assets/stylesheets/partials/*.scss'], {follow:true}, ['css']);
-        gulp.watch('src/assets/js/*.js', {follow:true}, ['js']);
+        gulp.watch('src/assets/scripts/*.js', {follow:true}, ['js']);
         gulp.watch(['src/*.jade', 'src/partials/*.jade', 'src/layouts/*.jade'], {follow:true}, ['templates']);
     });
 });
